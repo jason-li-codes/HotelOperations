cf12b65002a4a2ef8db5d280301a52301d5ca088
lijas <lijason991@gmail.com>
2025-10-19 23:05:51 -0400

    updated methods

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -3,9 +3,59 @@ package com.pluralsight;
 public class Room {
 
     private String type;
-    private double numberOfBeds;
+    private int numberOfBeds;
     private boolean isOccupied;
     private boolean isDirty;
-    private boolean isAvailable;
+
+    public Room(String type, int numberOfBeds) {
+        this.type = type;
+        this.numberOfBeds = numberOfBeds;
+        this.isOccupied = false;
+        this.isDirty = true;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    public boolean isAvailable() {
+        return !(this.isDirty || this.isOccupied);
+    }
+
+    public double getPricePerNight() {
+        return switch (this.type) {
+            case "king" -> 139.00;
+            case "double" -> 124.00;
+            default -> 0;
+        };
+    }
 
 }
